661f801d7a55cdaec3e829b1d795ea604a11b630
Michael Ting <mting4690@gmail.com>
2013-09-05 19:46:24 -0700

    garbage dna filtering enabled

--- get_unique_oligos.py
+++ get_unique_oligos.py
@@ -4,7 +4,7 @@
 Processes merged sequencing data from Illumina MiSeq
 
 Created 15 July 2013
-Updated 30 August 2013
+Updated 5 September 2013
 
 Copyright 2013 Michael Ting
 https://github.com/michaelting
@@ -41,6 +41,16 @@ class Counter(dict):
         maxIndex = values.index(max(values))
         return allitems[maxIndex][0]
         
+    def maxVal(self):
+        """
+        Returns the maximum value of counts across all keys
+        """
+        if len(self.keys()) == 0:
+            return None
+        allitems = self.items()
+        values = [v[1] for v in allitems]
+        return max(values)
+        
     def sortedKeys(self):
         """
         Returns a list of keys sorted by their values. 
@@ -72,95 +82,13 @@ def sign(x):
     else:
         return -1
         
-"""
-def sort_oligos(parser, tfunc, treepath):
-    
-    #Creates a directory tree with oligos sorted by person ID and oligo ID numbers
-    #in the given treepath
-    
-    
-    TAGLEN = 28  
-    
-    # try creating directories, if they already exist ignore the error
-    try:
-        os.makedirs(treepath)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
-    
-    badcharpattern  = re.compile('[^ACGT]')
-    tagpattern      = re.compile('TGTC[ACGT]{8}TGAT[ACGT]{12}')
-    tagformat       = re.compile('#\d{2}\$\d{3}')
-    
-    # look at each read in the sequencing file
-    for rec in parser:
-        
-        seqhead     = rec[0].strip()    # header
-        seqdna      = rec[1].strip()    # dna sequence
-        
-        # if sequence contains non-ATGC characters, skip it
-        findbadchars = badcharpattern.search(seqdna)
-        
-        if findbadchars:
-            continue
-        
-        # search for starting point of information
-        findtag = tagpattern.search(seqdna)
-
-        # exclue bad tags
-        if not findtag:
-            continue
-        
-        # check that tag is translated into correct format
-        tagcheck = tfunc(findtag.group())
-        checkformat = tagformat.match(tagcheck)
-        # exclude bad tags found in translation
-        if not checkformat:
-            continue
-        infostart = findtag.start()
-        
-        # correct the reading frame
-        seqdna = seqdna[infostart:]
-
-        # cap maximum length
-        if len(seqdna) > 104:
-            seqdna = seqdna[:104]
-        
-        # if length is not divisible by 4, throw it out
-        # occurs for sequences shorter than maximum length
-        if (len(seqdna) % 4) != 0:
-            continue
-        
-        # check the tag of the sequence       
-        tagdna  = seqdna[:TAGLEN]
-        msgdna  = seqdna[TAGLEN:]
-        person  = tagdna[4:12]
-        oligo   = tagdna[16:28]        
-        
-        pid     = tfunc(person)
-        oid     = tfunc(oligo)
-
-        # create storage for person if person not found
-        persondir = treepath + "/" + pid
-                
-        try:
-            os.makedirs(persondir)
-        except OSError as exception:
-            if exception.errno != errno.EEXIST:
-                raise
-
-        # create base consensus dict for oligo if oligo not found
-        oligodir = persondir + "/" + oid + ".txt"
-
-        try:
-            newfile = open(oligodir, "a")
-            newfile.write(msgdna+"\n")      # write only the message
-            newfile.close()
-        except:
-            raise
-"""
-
-def ram_sort(parser, tfunc):
+def sort_oligos(parser, tfunc):
+    """
+    Stores oligos by person ID and oligo ID in a nested dictionary structure
+    using counts of bases for each base position in each oligo by means of
+    a Counter object.
+    {{[]}}
+    """    
     
     TAGLEN = 28
     
@@ -169,7 +97,6 @@ def ram_sort(parser, tfunc):
     tagformat       = re.compile('#\d{2}\$\d{3}')
     
     #initialize RAM storage
-
     ramdict = {}
     
     # look at each read in the sequencing file
@@ -219,9 +146,7 @@ def ram_sort(parser, tfunc):
         
         pid     = tfunc(person)
         oid     = tfunc(oligo)
-
-        # create storage for person if person not found
-     
+   
         # check if person has been listed
         if pid not in ramdict:
             ramdict[pid] = {}
@@ -239,94 +164,49 @@ def ram_sort(parser, tfunc):
             ramdict[pid][oid][baseindex][base] += 1
 
     return ramdict
-        
-"""
-def get_consensus(treepath):
-    
-    #Modifies files in sorted oligo treepath to obtain consensus sequences
-    
-    # loop through each (person) in the treepath 
-    for root, dirs, filenames in os.walk(treepath):
-        for fname in filenames:
-            filepath = root + "/" + fname
-            # read oligos from one file
-            oligolist = []            
-            with open(filepath, "r") as currfile:
-                for line in currfile:
-                    oligolist.append(line.strip())
-                    
-            # count base occurrences at each position
-            counts = []
-            for oligo in oligolist:
-                for index in range(len(oligo)):
-                    
-                    base = oligo[index]
-                    # counter not yet initialized
-                    if index >= len(counts):
-                        counts.insert(index, Counter())
-                    # increase the count of the base
-                    counts[index][base] += 1
-                    
-            # determine the consensus sequence
-            consensus = ""
-            for poscounter in counts:
-                consensus += poscounter.argMax()
-            with open(filepath, "w") as consensusfile:
-                consensusfile.write(consensus+"\n")
-"""
 
-def ram_consensus(ramdict):
+def get_consensus(ramdict):
+    """
+    Determines consensus sequences using the base with the highest count
+    at each position.
+    """
+
+    COUNT_THRESHOLD = 100
+    badoligos = []
     
     for pid in ramdict:
         for oid in ramdict[pid]:
             # determine the consensus sequence of a particular pid, oid
             consensus = ""
             for poscounter in ramdict[pid][oid]:
-                consensus += poscounter.argMax()
+                # Counts below threshold imply erroneous sequences, since correct sequences
+                # are copied 100's-1000's of times
+                if poscounter.maxVal() < COUNT_THRESHOLD:
+                    # throw out the sequence below the threshold
+                    badpair = (pid, oid)
+                    badoligos.append(badpair)
+                    break
+                # Grab the base with the most counts
+                else:
+                    consensus += poscounter.argMax()
+            # replace the list of Counters with a consensus string
             ramdict[pid][oid] = consensus
+            
+    # remove bad oligos from the dictionary
+    for pair in badoligos:
+        badpers = pair[0]
+        badolig = pair[1]
+        del ramdict[badpers][badolig]            
             
     return ramdict
 
-"""
-def condense(treepath, tfunc):
-
-    #Condenses oligos into a single file and translates the result.
-    #Correct results are order dependent; Python's list sorting function
-    #will return the correct order:
-    #>>> ['012.txt','134.txt','179.txt','000.txt'].sort()
-    #['000.txt','012.txt','134.txt','179.txt']
+def condense(ramdict, tfunc, outdir):
+    """
+    Condense oligos into a single block of text based on pid,oid order, translates
+    DNA to ASCII, and writes output to files in outdir
+    """
     
-    for root, dirs, filenames in os.walk(treepath):
-
-        # skip directories with no files
-        if not filenames:
-            continue
-        
-        # sort files to ensure that condensed and translated files
-        # contain information in the correct order
-        filenames.sort()           
-        
-        # create condensed file which contains all oligos concatenated together
-        condensedfile = root + "/condensed.txt"
-        with open(condensedfile,"w") as condfile:
-            for fname in filenames:
-                filepath = root + "/" + fname
-                with open(filepath, "r") as currfile:
-                    for line in currfile:
-                        condfile.write(line.strip())
-                        
-        # translate condensed file into readable text
-        translatedfile = root + "/translated.txt"
-        with open(translatedfile, "w") as endfile:
-            with open(condensedfile,"r") as basefile:
-                for line in basefile:
-                    dna = line.strip()
-                    text = tfunc(dna)
-                    endfile.write(text+"\n")
-"""
-
-def ram_condense(ramdict, tfunc, outdir):    
-    
+    # create the output file directory
     try:
         os.makedirs(outdir)
     except OSError as exception:
@@ -341,14 +221,20 @@ def ram_condense(ramdict, tfunc, outdir):
         
         old_oid = "000"        
         
-        for oid in sorted(ramdict[pid]):
-            fullseq += ramdict[pid][oid]
+        for oid in sorted(ramdict[pid]): # sorts from 000,001,002,003,...
+        
+            fullseq += ramdict[pid][oid] # concatenate DNA into one block
     
             curr = int(oid)
             old  = int(old_oid)
+            # numerical skips imply erroneous sequences, since oligos have order
             if (curr - old) > 1:
                 break
             old_oid = oid
+        
+        # don't write files with sequences filtered out
+        if fullseq == "":
+            continue            
             
         # write to condensed file and translate
         with open(condensedfile, "w") as condfile:
@@ -363,32 +249,30 @@ def ram_condense(ramdict, tfunc, outdir):
 
 def main():
     
-    fqfile = gzip.open("merged.fastq.gz")   
-    parser = parse_fastq.readFastq(fqfile)
+    fqfile = gzip.open("merged.fastq.gz")
+    parser = parse_fastq.readFastq(fqfile)      # faster with generator
     
     translator = ASCIIcodons.DNAToText()
     translate_dna = translator.dna_to_text
     
-    treepath = "ramtestdna"
+    treepath = "decodeddna"
 
     print "Now sorting oligos..."
     sort_start = time.time()
-    rd = ram_sort(parser, translate_dna)
-    #sort_oligos(parser, translate_dna, treepath)
+    rd = sort_oligos(parser, translate_dna)
     sort_end = time.time()
     
     print "Retrieving consensus DNA sequences..."
     comb_start = time.time()
-    rd = ram_consensus(rd)
-    #get_consensus(treepath)
+    rd = get_consensus(rd)
     comb_end = time.time()
     
     print "Condensing DNA sequences and translating DNA to readable text..."
     cond_start = time.time()
-    ram_condense(rd, translate_dna, treepath)
-    #condense(treepath, translate_dna)
+    condense(rd, translate_dna, treepath)
     cond_end = time.time()
-    print "Run complete. See /###_condensed.txt and /###_translated.txt in /%s for results." % treepath
+    
+    print "Run complete. See /condensed.txt and /translated.txt in each subfolder of /%s for results." % treepath
     
     print "Elapsed time to sort was %g seconds" % (sort_end - sort_start)
     print "Elapsed time to combine was %g seconds" % (comb_end - comb_start)
